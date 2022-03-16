25e3f38f5c2b701c9a85f4019c4ff719d66a48d7
Bartusivaci <bartusivaci@gmail.com>
2022-03-16 17:59:21 +0300

    Assets are added.

--- Siege Rush/Assets/Scripts/EnemyMover.cs
+++ Siege Rush/Assets/Scripts/EnemyMover.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitDuration = 1f;
 
 
     void Start()
     {
-        PrintWaypointNames();
+        StartCoroutine(FollowPath());
     }
 
-    void PrintWaypointNames()
+    IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitDuration);
         }
     }
 }
