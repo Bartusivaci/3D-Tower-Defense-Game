b00015a5d676403b9821871df7087f39c77bd5a0
Bartusivaci <bartusivaci@gmail.com>
2022-03-18 11:57:47 +0300

    Tower placement is added.

--- Siege Rush/Assets/Scripts/EnemyMover.cs
+++ Siege Rush/Assets/Scripts/EnemyMover.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitDuration = 1f;
     [SerializeField] float yOffset = 5f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     void Start()
     {
@@ -22,13 +22,29 @@ public class EnemyMover : MonoBehaviour
             Vector3 startPosition = transform.position;
             Vector3 endPosition = waypoint.transform.position + addOffset;
             float travelPercent = 0f;
-            
+
+            float rotationDegrees = degreesToRotate(this.transform.forward, startPosition, endPosition);
+
+
+
             while(travelPercent < 1f)
             {
-                travelPercent += Time.deltaTime;
+                if (travelPercent < 0.25f)
+                {
+                    float rotationAngle = 4 * rotationDegrees * Time.deltaTime * speed;
+                    this.transform.Rotate(new Vector3(0, rotationAngle, 0));
+                }
+                travelPercent += Time.deltaTime * speed;
                 transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
                 yield return new WaitForEndOfFrame();
             }
         }
+    }
+
+
+    float degreesToRotate(Vector3 currentDir, Vector3 currentPosition, Vector3 endPosition)
+    {
+        Vector3 targetDir = endPosition - currentPosition;
+        return Vector3.SignedAngle(currentDir, targetDir, Vector3.up);
     }
 }
--- Siege Rush/Assets/Scripts/Waypoint.cs
+++ Siege Rush/Assets/Scripts/Waypoint.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] bool isPlaceable;
+    [SerializeField] GameObject towerPrefab;
+    [SerializeField] float towerOffset = 5f;
+
+    
+
+    void OnMouseDown()
     {
+        if (isPlaceable)
+        {
+            Vector3 addOffset = new Vector3(0f, towerOffset, 0f);
+            Instantiate(towerPrefab, transform.position + addOffset, Quaternion.identity);
+            isPlaceable = false;
+        }
         
+
     }
 }
